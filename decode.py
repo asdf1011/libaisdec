f405b22cc4800af5c1dac2984c99ab0b2841b904
Henry Ludemann <henry@supposition.(none)>
2011-07-25 23:14:47 +0800

    Allow converting from aivdm to binary.

---
+++ decode.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from bdec import DecodeError
+from bdec.spec import load_specs
+from bdec.output.instance import encode
+from bdec.output.xmlout import to_file
+import getopt
+import os.path
+import sys
+
+def usage(program):
+    print 'Decode aivdm encoded ais files.'
+    print 'Usage: %s [options] <filename.aivdm> ...'
+    print
+    print 'Options:'
+    print '  -h    Show this help.'
+
+class Decoder:
+    def __init__(self):
+        spec_dir = os.path.join(os.path.split(__file__)[0], 'spec')
+        self._aivdm = load_specs([os.path.join(spec_dir, 'aivdm.xml')])
+
+    def decode(self, data):
+        # Encode aivdm data back into binary
+        try:
+            bits = encode(self._aivdm[0], list(ord(c) for c in data.strip()))
+        except DecodeError, ex:
+            filename, line_number, column_number = self._aivdm[2][ex.entry]
+            sys.exit('%s[%i] - %s' %  (filename, line_number, ex))
+
+        # TODO: Decode the ais binary
+        print bits
+
+def main(args):
+    try:
+        opts, args = getopt.getopt(args[1:], 'h', [])
+    except getopt.GetOptError, ex:
+        sys.exit(ex)
+
+    for opt, value in opts:
+        if opt == '-h':
+            usage(args[0])
+        else:
+            raise NotImplementedError(opt)
+    
+    if not args:
+        sys.exit("No files to decode. Run '%s -h' for more details.", args[0])
+
+    decoder = Decoder()
+    for filename in args:
+        decoder.decode(file(filename, 'rb').read())
+
+if __name__ == '__main__':
+    main(sys.argv)
