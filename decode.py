08997071664274fedb13101a61d9725af9d34711
Henry Ludemann <henry@hl.id.au>
2011-07-27 22:23:18 +0800

    Correctly display ascii-6-bit fields.
    This is a hack, but it'll work for the python decode display.

--- decode.py
+++ decode.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from bdec import DecodeError
+from bdec.data import Data
+from bdec.field import Field
 from bdec.spec import load_specs, LoadError
 from bdec.output.instance import encode
 from bdec.output.xmlout import to_file
@@ -19,12 +21,41 @@ def usage(program):
 class Decoder:
     def __init__(self):
         spec_dir = os.path.join(os.path.split(__file__)[0], 'spec')
+        self._fake_6_bit_text_field = Field('6 bit ascii', length=0,
+                format=Field.TEXT)
 
         try:
             self._aivdm = load_specs([os.path.join(spec_dir, 'aivdm.xml')])
             self._ais = load_specs(glob(os.path.join(spec_dir, 'ais*.xml')))
         except LoadError, ex:
             sys.exit(str(ex))
+
+    def _filter_6_bit_ascii(self, items):
+        """Convert 6-bit ascii decode items into a single text field.
+        
+        This doesn't change the decoding, but hacks the decoded output
+        so it displays the data in a nicer fashion.
+
+        items -- An iterable list of decoded entries.
+        return -- A iterable list of decoded entries, 6-bit ascii items
+            collapsed into a single field."""
+        iterable = iter(items)
+        for is_starting, name, entry, data, value in iterable:
+            if is_starting and entry.name == '6 bit ascii':
+                # We found a 6-bit ascii string. Get the data and values for
+                # the child entries, and collapse them into a single result.
+                yield True, name, self._fake_6_bit_text_field, None, None
+                text = []
+                d = Data()
+                for is_starting, name, entry, data, value in iterable:
+                    if entry.name == '6 bit ascii':
+                        break
+                    elif value is not None and name=='character':
+                        d += data
+                        text += chr(value)
+                yield False, name, self._fake_6_bit_text_field, d, ''.join(text)
+            else:
+                yield is_starting, name, entry, data, value
 
     def decode(self, data):
         # Encode aivdm data back into binary
@@ -36,7 +67,7 @@ class Decoder:
 
         # Decode the ais binary
         try:
-            to_file(self._ais[0].decode(bits), sys.stdout)
+            to_file(self._filter_6_bit_ascii(self._ais[0].decode(bits)), sys.stdout)
         except DecodeError, ex:
             filename, line_number, column_number = self._ais[2][ex.entry]
             sys.exit('%s[%i] - %s' %  (filename, line_number, ex))
